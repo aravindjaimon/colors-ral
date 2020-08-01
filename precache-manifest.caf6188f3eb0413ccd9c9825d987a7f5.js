8a20b5e9ff7ef05a985ab5151336da6314092ac6
Aravind Jaimon <aravindjaimon1@gmail.com>
2020-08-01 14:48:25 +0530

    Updates

---
+++ precache-manifest.caf6188f3eb0413ccd9c9825d987a7f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcaa5b6519a5c2ccf6c81130a458b2e",
+    "url": "/colors-ral/index.html"
+  },
+  {
+    "revision": "f1120483c3491d1cae97",
+    "url": "/colors-ral/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "67edbd64bec65d0c67fb",
+    "url": "/colors-ral/static/css/main.3365be15.chunk.css"
+  },
+  {
+    "revision": "f1120483c3491d1cae97",
+    "url": "/colors-ral/static/js/2.5493e025.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/colors-ral/static/js/2.5493e025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67edbd64bec65d0c67fb",
+    "url": "/colors-ral/static/js/main.77668f35.chunk.js"
+  },
+  {
+    "revision": "163e4fae8f9dacc99530",
+    "url": "/colors-ral/static/js/runtime-main.19ed8e84.js"
+  }
+]);
